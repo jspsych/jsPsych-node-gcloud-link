90253e6a9e7a57ce666eb7d4c1ff305ce1086fde
Josh de Leeuw <josh.deleeuw@gmail.com>
2016-05-19 16:17:59 -0400

    first working version

--- index.js
+++ index.js
@@ -6,23 +6,34 @@ module.exports = {
 
   connect: function(opts){
     projectID = opts.projectID;
+    keyFile = opts.key;
     datastore = gcloud.datastore({
-      projectId: projectID
+      projectId: projectID,
+      keyFilename: keyFile
     });
-  }
+  },
 
   // the write_msg has two properties
   // collection: the collection/table where the data should be saved
   // data: the data to save
   write: function(write_msg){
-    var key = datastore.key(write_msg.collection);
+    console.log(JSON.stringify(write_msg));
     var data = write_msg.data;
     if(!Array.isArray(data)){
       data = [data];
     }
-    datastore.save(data, function(err){
-      console.log('Error writing data in gcloud link. Key: '+key);
-    });
+    for(var i=0;i<data.length;i++){
+      var key = datastore.key(write_msg.collection);
+      datastore.save({
+        key: key,
+        data: data[i]
+      }, function(err){
+        if(err!=null){
+          console.log(err);
+        }
+        //console.log('Error writing data in gcloud link. Key: '+key);
+      });
+    }
   }
 
 }
